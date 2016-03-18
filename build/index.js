d20de9df897663a2e378814c7e0d3d46d0975686
TJ Holowaychuk <tj@vision-media.ca>
2016-03-17 18:50:05 -0700

    build

--- build/index.js
+++ build/index.js
@@ -16,6 +16,8 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+// TODO: path gradients are a bitch
+
 /**
  * Default config.
  */
@@ -25,10 +27,10 @@ var defaults = {
   target: '#chart',
 
   // width of chart
-  width: 450,
+  width: 550,
 
   // height of chart
-  height: 130,
+  height: 170,
 
   // margin
   margin: { top: 15, right: 0, bottom: 35, left: 60 },
@@ -37,27 +39,33 @@ var defaults = {
   axisPadding: 5,
 
   // axis tick size
-  tickSize: 10,
+  tickSize: 0,
 
-  // padding between bars
-  barPadding: 13,
+  // number of x-axis ticks
+  xTicks: 5,
+
+  // number of y-axis ticks
+  yTicks: 3,
 
   // nice round values for axis
-  nice: true
+  nice: false,
+
+  // line interpolation
+  interpolate: 'basis'
 };
 
 /**
- * BarChart.
+ * LineChart.
  */
 
-var BarChart = function () {
+var LineChart = function () {
 
   /**
    * Construct with the given `config`.
    */
 
-  function BarChart(config) {
-    _classCallCheck(this, BarChart);
+  function LineChart(config) {
+    _classCallCheck(this, LineChart);
 
     this.set(config);
     this.init();
@@ -67,7 +75,7 @@ var BarChart = function () {
    * Set configuration options.
    */
 
-  _createClass(BarChart, [{
+  _createClass(LineChart, [{
     key: 'set',
     value: function set(config) {
       for (var k in defaults) {
@@ -98,12 +106,17 @@ var BarChart = function () {
   }, {
     key: 'init',
     value: function init() {
+      var _this = this;
+
       var target = this.target;
       var width = this.width;
       var height = this.height;
       var margin = this.margin;
       var axisPadding = this.axisPadding;
+      var interpolate = this.interpolate;
       var tickSize = this.tickSize;
+      var xTicks = this.xTicks;
+      var yTicks = this.yTicks;
 
       var _dimensions = this.dimensions();
 
@@ -119,13 +132,21 @@ var BarChart = function () {
 
       this.y = _d3.default.scale.linear().range([h, 0]);
 
-      this.xAxis = _d3.default.svg.axis().orient('bottom').scale(this.x).ticks(5).tickPadding(8).tickSize(tickSize);
+      this.xAxis = _d3.default.svg.axis().orient('bottom').scale(this.x).ticks(xTicks).tickPadding(8).tickSize(tickSize);
 
-      this.yAxis = _d3.default.svg.axis().orient('left').scale(this.y).ticks(3).tickPadding(8).tickSize(tickSize);
+      this.yAxis = _d3.default.svg.axis().orient('left').scale(this.y).ticks(yTicks).tickPadding(8).tickSize(tickSize);
 
       this.chart.append('g').attr('class', 'x axis').attr('transform', 'translate(0, ' + (h + axisPadding) + ')').call(this.xAxis);
 
       this.chart.append('g').attr('class', 'y axis').attr('transform', 'translate(' + -axisPadding + ', 0)').call(this.yAxis);
+
+      this.line = _d3.default.svg.line().x(function (d) {
+        return _this.x(d.time);
+      }).y(function (d) {
+        return _this.y(d.value);
+      }).interpolate(interpolate);
+
+      this.chart.append('path').attr('class', 'line');
     }
 
     /**
@@ -171,7 +192,6 @@ var BarChart = function () {
       var chart = this.chart;
       var x = this.x;
       var y = this.y;
-      var barPadding = this.barPadding;
 
       var _dimensions3 = this.dimensions();
 
@@ -181,64 +201,37 @@ var BarChart = function () {
       var h = _dimensions4[1];
 
 
-      var colWidth = w / data.length - barPadding;
-      if (colWidth < 1) throw new Error('BarChart is too small for the amount of data provided');
-
       var column = chart.selectAll('.column').data(data);
 
       // enter
       column.enter().append('rect').attr('class', 'column');
 
       // update
-      column.attr('x', function (d) {
-        return x(d.time);
-      }).attr('rx', colWidth / 2).attr('ry', colWidth / 2).attr('width', colWidth).attr('y', 0).attr('height', function (d) {
+      column.attr('width', 1).attr('height', function (d) {
         return h;
-      });
+      }).attr('x', function (d) {
+        return x(d.time);
+      }).attr('y', 0);
 
       // exit
       column.exit().remove();
     }
 
     /**
-     * Render bars.
+     * Render line.
      */
 
   }, {
-    key: 'renderBars',
-    value: function renderBars(data) {
-      var chart = this.chart;
-      var x = this.x;
-      var y = this.y;
-      var barPadding = this.barPadding;
-
-      var _dimensions5 = this.dimensions();
-
-      var _dimensions6 = _slicedToArray(_dimensions5, 2);
-
-      var w = _dimensions6[0];
-      var h = _dimensions6[1];
+    key: 'renderLine',
+    value: function renderLine(data) {
+      var chart = this.chart.transition();
+      var line = this.line;
 
 
-      var barWidth = w / data.length - barPadding;
-      if (barWidth < 1) throw new Error('BarChart is too small for the amount of data provided');
+      chart.select('.line').attr('d', line(data));
 
-      var bar = chart.selectAll('.bar').data(data);
-
-      // enter
-      bar.enter().append('rect').attr('class', 'bar');
-
-      // update
-      bar.attr('x', function (d) {
-        return x(d.time);
-      }).attr('rx', barWidth / 2).attr('ry', barWidth / 2).attr('width', barWidth).transition().attr('y', function (d) {
-        return y(d.value);
-      }).attr('height', function (d) {
-        return h - y(d.value);
-      });
-
-      // exit
-      bar.exit().remove();
+      // hack: fixes order
+      chart.node().appendChild(chart.select('.line').node());
     }
 
     /**
@@ -252,7 +245,7 @@ var BarChart = function () {
 
       this.renderAxis(data, options);
       this.renderCols(data, options);
-      this.renderBars(data, options);
+      this.renderLine(data, options);
     }
 
     /**
@@ -268,7 +261,7 @@ var BarChart = function () {
     }
   }]);
 
-  return BarChart;
+  return LineChart;
 }();
 
-exports.default = BarChart;
+exports.default = LineChart;
